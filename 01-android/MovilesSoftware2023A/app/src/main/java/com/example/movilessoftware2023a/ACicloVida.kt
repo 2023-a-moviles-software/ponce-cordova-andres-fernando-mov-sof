5156a5e39700ab7468312835d4d979547cf21867
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-06-08 10:55:47 -0500

    Clase 10 Android Studio
    Recursos y Actividades

---
+++ 01-android/MovilesSoftware2023A/app/src/main/java/com/example/movilessoftware2023a/ACicloVida.kt
@@ -0,0 +1,13 @@
+package com.example.movilessoftware2023a
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+}
+
--- 01-android/MovilesSoftware2023A/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-android/MovilesSoftware2023A/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.movilessoftware2023a
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Se crea una variable para el botón ciclo de vida
+        val botonACicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+
+        //Se añade un listener al botón ciclo de vida
+        botonACicloVida.setOnClickListener {
+            irActividad(ACicloVida::class.java)
+        }
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
